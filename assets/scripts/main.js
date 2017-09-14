6a78deb3919108b533043288d2de02de3a533392
CMunkyDev <matt@jcmueller.com>
2017-09-14 12:57:24 -0700

    keep data before branching

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,17 @@
 //tabIDString should be 'tab#' where # is the number of the tab
 var allTabs = document.getElementsByClassName("project-tab");
+var allHeaders = document.getElementsByClassName("tab-header");
+
+// for (let i  = 0; i < allTabs.length; i++) {
+//   allHeaders[i] = allTabs[i].querySelector('.tab-header');
+// }
 
 var tabOrderObj = {};
 var tabOrderArr = [];
 var tabOpenedToObj = {};
+var tabHeightObj = {};
+var open;
+var close;
 
 for (let i = 0; i < allTabs.length; i++) {
   tabOrderObj[allTabs[i].id] = i;
@@ -18,28 +26,39 @@ function convertPxString (pixelString) {
   }
 }
 
-function openTab (tabIDString, amountToOpenPx = 2000, tabOpenedToPxObj = tabOpenedToObj, pxPer = 5, intervalLength = 5) {
+function tabBottomPos (tabIDString, tabOpenedToPxObj = tabOpenedToObj) {
+  let returnVal = tabOpenedToPxObj[tabIDString];
+  if (returnVal === undefined) {
+    return 0;
+  } else {
+    return returnVal;
+  }
+}
+
+function openTab (tabIDString, amountToOpenPx = 2000, tabOpenedToPxObj = tabOpenedToObj, pxPer = 10, intervalLength = 10) {
+  clearInterval(close);
+  clearInterval(open);
   let tabStyle = document.getElementById(tabIDString).style;
-  let offset = 0;
-  let pastVal = 0;
-  let open = setInterval(moveDown, intervalLength);
+  let offset = tabBottomPos(tabIDString, tabOpenedToPxObj);
+  open = setInterval(moveDown, intervalLength);
   function moveDown () {
-    console.log(offset);
     if (convertPxString(tabStyle.bottom) <= -amountToOpenPx) {
+      let offset = 0;
       clearInterval(open);
     } else {
       offset -= pxPer;
-      pastVal = offset;
       tabStyle.bottom = offset.toString() + 'px';
     }
     tabOpenedToPxObj[tabIDString] = offset;
   }
 }
 
-function closeTab (tabIDString, tabOpenedToPxObj = tabOpenedToObj, pxPer = 5, intervalLength = 5) {
+function closeTab (tabIDString, tabOpenedToPxObj = tabOpenedToObj, pxPer = 10, intervalLength = 10) {
+  clearInterval(open);
+  clearInterval(close);
   let tabStyle = document.getElementById(tabIDString).style;
-  let offset = tabOpenedToPxObj[tabIDString];
-  let close = setInterval(moveUp, intervalLength);
+  let offset = tabBottomPos(tabIDString, tabOpenedToPxObj);
+  close = setInterval(moveUp, intervalLength);
   function moveUp () {
     if (convertPxString(tabStyle.bottom) >= 0) {
       offset = 0;
@@ -50,4 +69,14 @@ function closeTab (tabIDString, tabOpenedToPxObj = tabOpenedToObj, pxPer = 5, in
     }
     tabOpenedToPxObj[tabIDString] = offset;
   }
+}
+
+function openCloseDecide (tabIDString, tabOpenedToPxObj = tabOpenedToObj) {
+  tabBottomPos(tabIDString, tabOpenedToPxObj) ? closeTab(tabIDString) : openTab(tabIDString);
+}
+
+for (let i = 0; i < allHeaders.length; i++) {
+  allHeaders[i].addEventListener('click', function (event) {
+    openCloseDecide(event.target.parentNode.id, tabOpenedToObj);
+  })
 }
